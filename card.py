8920693ca7a226dc90e1517e60fe37b6785bc514
Eric H <eboy.coltt5@gmail.com>
2020-03-04 16:08:44 -0800

    Add image and header to pdf

--- card.py
+++ card.py
@@ -1,15 +1,26 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import LETTER
+from reportlab.lib.units import inch as INCH
 
-def hello(c):
-    c.drawString(100, 100, "Example")
+def hello(c, width, height, description=""):
+    # Image should be centered, and take up about 2/3 of the page width
+    # Actual values are for 1/5 page width margin on either side, 1 inch top margin
+    # Image height capped at 1.5 inches
+    c.drawImage(".\\images\\logo.png", width / 5, height - INCH,
+        width=(width * 3 / 5),
+        height=-(INCH * 2),
+        preserveAspectRatio=True
+    )
+    # Print the control card text
+    c.setFontSize(36)
+    c.drawCentredString(width / 2, height - INCH * 4, "Control Card")
+    # Description
 c = canvas.Canvas(
     ".\\output\\hello.pdf",
-    pagesize=LETTER,
-
+    pagesize=LETTER
 )
 width, height = LETTER
 print("Width:", width, "Height:", height)
-hello(c)
+hello(c, width, height)
 c.showPage()
 c.save()
